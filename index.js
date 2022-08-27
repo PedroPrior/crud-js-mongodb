d0630ff7d0b2b102066afe8d037b7c58ed157f2b
Pedro Henrique <pedroprior77@gmail.com>
2022-08-27 17:47:38 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,133 @@
+
+// Configuração Inicial
+const express = require('express')
+const app = express()
+
+// Banco de dados e Entity
+const mongoose = require('mongoose')
+const Person = require('./models/Person')
+
+
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+// Rotas (CRUD)
+
+// POST
+app.post('/person', async (req, res) => {
+  const { name, salary, approved } = req.body
+
+  const person = {
+    name,
+    salary,
+    approved,
+  }
+
+  try {
+    await Person.create(person)
+
+    res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+// GET
+app.get('/person', async (req, res) => {
+  try {
+    const people = await Person.find()
+
+    res.status(200).json(people)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+// GET by ID
+
+app.get('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const person = await Person.findOne({ _id: id })
+
+    if (!person) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+
+// UPDATE
+
+app.patch('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  const { name, salary, approved } = req.body
+
+  const person = {
+    name,
+    salary,
+    approved,
+  }
+
+  try {
+    const updatedPerson = await Person.updateOne({ _id: id }, person)
+
+    if (updatedPerson.matchedCount === 0) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+
+// DELETE
+
+app.delete('/person/:id', async (req, res) => {
+  const id = req.params.id
+
+  const person = await Person.findOne({ _id: id })
+
+  if (!person) {
+    res.status(422).json({ message: 'Usuário não encontrado!' })
+    return
+  }
+
+  try {
+    await Person.deleteOne({ _id: id })
+
+    res.status(200).json({ message: 'Usuário removido com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Oi Express!' })
+})
+
+// Conexão banco
+
+mongoose.connect('mongodb+srv://pedro:root@apicluster.saprc30.mongodb.net/bancoapi?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Conectado com sucesso!')
+    app.listen(3000)
+})
+.catch((err) => console.log(err)) 
+
